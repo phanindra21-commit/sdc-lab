89d78948c699aaac917a8fed4f1886593123e546
phanindra21-commit <ogiralaphanindra21@gmail.com>
2026-04-26 19:10:05 +0530

    Initial setup: created login form for servlet integration

---
+++ exp7/LoginServlet.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
+
+public class LoginServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection con = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/studentdb",
+                "root",
+                "password"
+            );
+
+            // ✔ Using PreparedStatement (IMPORTANT FIX)
+            PreparedStatement ps = con.prepareStatement(
+                "SELECT * FROM users WHERE username=? AND password=?"
+            );
+
+            ps.setString(1, username);
+            ps.setString(2, password);
+
+            ResultSet rs = ps.executeQuery();
+
+            if(rs.next()){
+                out.println("<h3>Login Successful</h3>");
+            } else {
+                out.println("<h3>Invalid Username or Password</h3>");
+            }
+
+            con.close();
+
+        } catch(Exception e){
+            out.println(e);
+        }
+    }
+}
