eb0661af3716924399e44f268f6f49a26280a82d
phanindra21-commit <ogiralaphanindra21@gmail.com>
2026-04-26 18:39:07 +0530

    Added experiment 3: JavaScript login form validation

---
+++ exp3/script.js
@@ -0,0 +1,19 @@
+function validateForm(){
+
+let username = document.getElementById("username").value;
+let password = document.getElementById("password").value;
+
+if(username.trim() === ""){
+    alert("Username cannot be empty");
+    return false;
+}
+
+if(password.length < 6){
+    alert("Password must be at least 6 characters");
+    return false;
+}
+
+alert("Login successful");
+return true;
+
+}
